b1e74af5fc1f4c9271521afd075766621419f069
marcelcorso <marcel.corso@gmail.com>
2011-05-28 12:55:57 +0200

    bang

--- revolution.js
+++ revolution.js
@@ -8,7 +8,6 @@ var Revolution = function() {
 
   that.init = function() {
     that.preload_music();
-    that.load_lyrics();
     that.preload_mp3s();
   }
 
@@ -16,14 +15,10 @@ var Revolution = function() {
     // load mp3 on an audio tag
   }
 
-  that.preload_lyrics = function() {
-    // create an array with the lyrics
-  }
-
   that.preload_mp3s = function() {
     // iterates the lyrics and creates preloaded audio tags with the google tts urls
   }
 
   return that;
-}
+};
 
