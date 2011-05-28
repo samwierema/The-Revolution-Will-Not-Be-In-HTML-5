a57f8a5b67676f83e87ff552412400dd7da8e625
marcelcorso <marcel.corso@gmail.com>
2011-05-28 14:52:45 +0200

    play shit on time, brother

--- revolution.js
+++ revolution.js
@@ -9,18 +9,43 @@ var Revolution = function() {
   that.init = function() {
     that.preload_music();
     that.preload_mp3s();
-  }
+  };
 
   that.preload_music = function() {
 	  that.audio = document.createElement('audio');
     that.audio.src = 'audio/the-revolution-will-not-be-televized.mp3';
-    that.audio.preload = 'auto';
+    that.audio.load();
+    $(that.audio).bind('play', that.fire_audio_phrases);
     that.audio.play();
-  }
+  };
 
   that.preload_mp3s = function() {
     // iterates the lyrics and creates preloaded audio tags with the google tts urls
+    that.audio_phrases = [];
+    for(var i = 0; i < lyrics.length; i++) {
+      var timestamp = lyrics[i][0];
+      var phrase = lyrics[i][1]
+      var a = document.createElement('audio');
+      a.src = 'http://translate.google.com/translate_tts?tl=en&q=' + escape(phrase);
+      a.load();
+      that.audio_phrases.push([timestamp, phrase, a]);
+    }
+  };
+
+
+  that.fire_audio_phrases = function() { 
+    for(var i = 0; i < that.audio_phrases.length; i++) {
+      var timestamp = 3 * that.audio_phrases[i][0];
+      setTimeout("revolution.fire_phrase(" + i + ")", timestamp);
+    }
+  };
+
+  that.fire_phrase = function(position) {
+    var bundle = that.audio_phrases[position];
+    $('#phrase').text(bundle[1]);
+    bundle[2].play();
   }
+
 
   return that;
 };
